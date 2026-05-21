8652bbf1e66379e6aa9d696d739a5c1bb033edb3
Justin Hayes <justinbhayes@gmail.com>
2026-05-21 11:05:27 -0500

    More performance fixes

--- src/data/buildInfo.js
+++ src/data/buildInfo.js
@@ -1,2 +1,2 @@
 // Last git commit timestamp - updated automatically by scripts/generate-sitemap.mjs
-export const lastCommitTime = new Date("2026-05-20T16:57:44-05:00");
+export const lastCommitTime = new Date("2026-05-21T10:48:37-05:00");
--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import Layout from "../components/Layout";
 import Seo from "../components/Seo";
-import "../styles/bootstrap-theme.scss";
 
 let emailjsClient;
 let emailjsInitialized = false;
