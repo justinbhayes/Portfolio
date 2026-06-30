2d6133cf5507a62323d0fc1493a1bf8a65f7f3e5
Justin Hayes <justinbhayes@gmail.com>
2026-06-30 10:03:43 -0500

    Updated build info

--- src/data/buildInfo.js
+++ src/data/buildInfo.js
@@ -1,2 +1,2 @@
 // Last git commit timestamp - updated automatically by scripts/generate-sitemap.mjs
-export const lastCommitTime = new Date("2026-06-08T15:51:21-05:00");
+export const lastCommitTime = new Date("2026-06-30T09:48:25-05:00");
